529ac80c158d6d5c84a095c1acca1f14f8e9f63a
BanditCook <tanguy.lalexandre@gmail.com>
2024-02-02 09:23:41 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,5 +13,7 @@ def checkout(request):
     template= 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OfITvIjZUE8b8QfqiqeO9n519V4er3WFFmSUDi0UHoOUevhIASSG1KLqDYYzXdz4VxPh539TtCaEk6H4aoj28wO00BfylN21c',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
